6cb8fcb93d47ea2b96f22a2cc8cff48a0bedc18b
Juan Gutierrez Gutierrez <gurrunyo1@gmail.com>
2023-10-03 12:13:26 +0200

    Se ha eliminado la linea de los apellidos y se ha añadido una linea con mi grupo de musica favorito.

--- Prueba.java
+++ Prueba.java
@@ -2,8 +2,8 @@ public class Prueba {
 
 	public static void main(String args[]){
 
-		System.out.println("Gutierrez Gutierrez");
 		System.out.println("25 años - 1998");
 		System.out.println("Ensalada de pasta");
+		System.out.println("Have a nice life");
 	}
 }
